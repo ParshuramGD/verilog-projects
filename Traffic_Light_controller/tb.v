d0bfe223a264302070d99a6b8ebca2cb93a89b51
ParshuramGD <parshuram.deshpande23@vit.edu>
2025-10-01 12:37:28 +0530

    Added Traffic light controller project folder

---
+++ Traffic_Light_controller/tb.v
@@ -0,0 +1,22 @@
+// Verilog code for Test bench
+// Define module
+module tb;
+reg clock = 0; // Define all I/O ports
+wire red, yellow, green;
+
+// Map all th I/O ports with DUT
+traffic_light_controller uut(
+    .clock(clock),
+    .red(red),
+    .yellow(yellow),
+    .green(green)
+);
+
+
+  // Clock generation
+  always #10 clock = ~clock;
+
+
+
+
+endmodule
